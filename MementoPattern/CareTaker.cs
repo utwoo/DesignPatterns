3a38c299cf36f715529c7fd52bb6b77b393b11bf
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-06-29 12:36:33 +0800

    Memento Pattern

---
+++ MementoPattern/CareTaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MementoPattern
+{
+    public class CareTaker
+    {
+        private readonly List<Memento> _mementoList = new List<Memento>();
+
+        public void Add(Memento state)
+        {
+            _mementoList.Add(state);
+        }
+
+        public Memento Get(int index)
+        {
+            return _mementoList[index];
+        }
+    }
+}
---
+++ MementoPattern/Memento.cs
@@ -0,0 +1,17 @@
+﻿namespace MementoPattern
+{
+    public class Memento
+    {
+        private readonly string _state;
+
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        public string GetState()
+        {
+            return _state;
+        }
+    }
+}
